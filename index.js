4769eea212b9a53795ddcec8e4396e4ee33cae5a
lenguyenchinguyen <145195459+lenguyenchinguyen@users.noreply.github.com>
2024-03-13 17:03:02 +0700

    Lab2

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World!!");
+var blog = [
+    "Hello world 1",
+    "Hello world 2"
+];
+blog.forEach(function (element) {
+    console.log(element);
+});
+// gợi ý lưu file tự động chuyển 
+// cách 1 : sử dụng ext để phiên dịch
+// cách 2 : sử glup
+// cách 3 : sử dụng npm-watch
+var number1 = 1;
+var number2 = 1;
+console.log(number1 + number2);
